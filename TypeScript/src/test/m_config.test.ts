31116bd182737bcf7824f129cdd449988b3b2abd
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2025-02-10 04:15:10 +0200

    Fixed m_config

--- TypeScript/src/test/m_config.test.ts
+++ TypeScript/src/test/m_config.test.ts
@@ -1,13 +1,13 @@
 import M_Config from '../m_config';
 import { M_Logging } from '../m_logging';
-import { PythonMessage, PythonScript, python_message_type } from '../python_message';
+import { PythonMessage} from '../python_message';
 
 describe('readTsConfig', () => {
   test("should read and parse ty_call_py.json file", async () => {
     console.log("Main app:", M_Config.main_app);
     console.log("Config:", M_Config.config);
     console.log("Main con:", M_Config.main_con.child.pid);
-    const m_python_message = new PythonMessage( python_message_type.m_json);
+    const m_python_message = new PythonMessage( "test1", {});
     await M_Config.main_con.send(m_python_message);
     await M_Config.main_con.sendStr("Hello from VS Code!");
     await M_Config.destroy();
